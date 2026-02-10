c44e4d16bcb4c27197bea08c3df77b438c5f2fd5
ashish123256 <ashish.1911rkt@gmail.com>
2026-02-10 14:14:28 +0530

    fix bug

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -2,28 +2,24 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { config } from 'dotenv';
 
+
+
 config();
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
+  host: process.env.DB_HOST ,
+  port: parseInt(process.env.DB_PORT) ,
+  username: process.env.DB_USERNAME ,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_DATABASE ,
+  
 
-  // Railway / Production
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-
-  entities: ['dist/**/*.entity.js'],
+   entities: ['dist/**/*.entity.js'],
   synchronize: false,
   logging: process.env.DB_LOGGING === 'true',
-
-  // REQUIRED on Railway Postgres
-  ssl: process.env.NODE_ENV === 'production'
-    ? { rejectUnauthorized: false }
-    : false,
 };
 
 const dataSource = new DataSource(dataSourceOptions);
+
 export default dataSource;
